c6143f94e5546398e463a50d788d6d32cc44e4b4
christianalim <christianalim1@gmail.com>
2019-08-10 16:42:28 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let win = array.find( function(s){
+    if(s['result'] === 'W'){
+      return s.year
+    } else {
+      return undefined
+    }
+  })
+  if(win){
+    return win.year
+  }
+}
+ 
