c5839c9662745cc1c9cbbebc3a2221a1b2a8a799
github username <github email address>
2019-08-11 10:39:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-// const testVar = {}
-
-// function testFunc() {
-//   return "hi"
-// }
-
 const record = [
   {year: "2018", result: "L"},
   {year: "2017", result: "W"},
   {year: "2016", result: "N/A"}
   //...
 ]
+
+// Create a function called superBowlWin to take in argument of an array
+// use .find()
+// if the element's result was a "w" then return that elements year
+// else return 'undefined'
 
 const superBowlWin = (record) => {
   record.find(record => {
@@ -21,4 +20,5 @@ const superBowlWin = (record) => {
 })
 } 
 
+// Invoking the function
 superBowlWin(record)
