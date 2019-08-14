3d86d6df49c47db74956dcfd3ad7566ef034cfa8
Adrian M Popan <adrian.popan@gmail.com>
2019-08-14 09:36:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const testVar = {}
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+// ]
 
-function testFunc() {
-  return "hi"
-}
+function superbowlWin(record) {
+  let winning = record.find( obj => obj.result === "W")
+  return winning ? winning.year : undefined
+};
+
+superbowlWin(record);
